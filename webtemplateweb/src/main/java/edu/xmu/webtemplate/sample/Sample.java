755118f0a4964d56872bcbcd0d2a73772f7b6970
kunlun.ykl <kunlun.ykl@alibaba-inc.com>
2017-10-19 15:47:57 +0800

    updated basic package

---
+++ webtemplateweb/src/main/java/edu/xmu/webtemplate/sample/Sample.java
@@ -0,0 +1,10 @@
+package edu.xmu.webtemplate.sample;
+
+/**
+ * @author davywalker
+ */
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
